ccb50c7054d8f0b9ad27da316549378e214a3db9
Bill Swartz <panchocoquito@gmail.com>
2026-01-26 18:17:26 -0500

    Update 01_DebugBasics.ino
    Added unformatted comments

--- examples/01_DebugBasics/01_DebugBasics.ino
+++ examples/01_DebugBasics/01_DebugBasics.ino
@@ -1,20 +1,22 @@
-#define DEBUG 1
-#define DEBUG_MOTOR 1
+#define DEBUG 1 // TODO: check formatting
+#define DEBUG_MOTOR 1 // TODO: list all options
 
 #include <DefLab_Common.h>
 
 void setup() {
+// Initiate debug and choose a baud rate
   DL_DEBUG_BEGIN(115200);
 }
 
 void loop() {
+// Choose a category and print interval in ms
   DL_DEFINE_DEBUG_CATEGORY_LOCAL(MOTOR, 500);
 
   static int x = 0;
   x++;
 
-  DL_DUMP_CAT(MOTOR, {
-    debug("x=");
-    debugln(x);
+  DL_DUMP_CAT(MOTOR, { // first param is your category
+    debug("x=");       // debug prints without a new line
+    debugln(x);        // debugln ends in a carriage return
   });
-}
+} // TODO: edit this in IDE 
