f8dc52c0c73adb457d18b799b764b2e750779805
Bill Swartz <panchocoquito@gmail.com>
2026-01-26 20:30:26 -0500

    Refactor debug setup and print functionality
    Updated debug macros and improved comments for clarity.

--- examples/01_DebugBasics/01_DebugBasics.ino
+++ examples/01_DebugBasics/01_DebugBasics.ino
@@ -1,22 +1,24 @@
-#define DEBUG 1 // TODO: check formatting
-#define DEBUG_MOTOR 1 // TODO: list all options
+#define DEBUG_MOTOR 1  // see DefLab_Common.h for full macro list
 
-#include <DefLab_Common.h>
+#include <DefLab_Common.h>  // add this library after the macro
 
 void setup() {
-// Initiate debug and choose a baud rate
+  // Initiate debug and choose a baud rate
   DL_DEBUG_BEGIN(115200);
 }
 
 void loop() {
-// Choose a category and print interval in ms
+  // Choose a category and print interval in ms
   DL_DEFINE_DEBUG_CATEGORY_LOCAL(MOTOR, 500);
 
   static int x = 0;
   x++;
 
-  DL_DUMP_CAT(MOTOR, { // first param is your category
-    debug("x=");       // debug prints without a new line
-    debugln(x);        // debugln ends in a carriage return
-  });
-} // TODO: edit this in IDE 
+  // Call the function to print at the interval you provided
+  DL_DUMP_CAT(      // DL_DUMP_CAT accesses any of the categories
+    MOTOR,          // first param is your category
+    {               // then whatever you want in the body
+      debug("x=");  // 'debug' prints without a new line
+      debugln(x);   // 'debugln' adds a carriage return
+    });             // be sure to close parens and add ;
+}
